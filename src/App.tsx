50db7a648e4389c707c91983c7b272bf7cb330d4
root <root@LAPTOP-3N0COPU5.>
2025-03-09 22:43:55 +0000

    add login page

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import BackGroundImage2x from "/Background2x.png";
+import BackGroundImage from "/Background.png";
+import Tree from "/Tree.png";
+import Tree2x from "/Tree2x.png";
+import ErrorSVG from "./assets/Error.png";
+import ErrorSVG2x from "./assets/Error2x.png";
+
+import OAuthButton from "./components/OauthButton";
+import { useEffect, useState } from "react";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const validEmail = (email: string) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  };
+
+  const Error = () => {
+    return (
+      <div className="flex gap-x-1 items-center mt-1.5 myriadpro">
+        <img src={ErrorSVG} alt="Small" className="block sm:hidden w-4 h-4" />
+        <img src={ErrorSVG2x} alt="Large" className="hidden sm:block w-4 h-4" />
+        <p className="text-[#EE2D6E] text-[13px]">
+          The email field is required
+        </p>
+      </div>
+    );
+  };
+
+  return (
+    <main className="flex justify-center josefin-sans-400 w-full">
+      <img
+        src={BackGroundImage}
+        alt="Small"
+        className="block sm:hidden fixed -z-10"
+      />
+      <img
+        src={BackGroundImage2x}
+        alt="Large"
+        className="hidden sm:block fixed -z-10"
+      />
+      <div className="sm:w-[577px] w-[90%] bg-white mt-[81px]">
+        <div
+          className="h-[58px]"
+          style={{ boxShadow: "0px 3px 7px #00000029" }}
+        ></div>
+        <div className="flex flex-col items-center sm:w-[385px] max-w-[385px] w-full m-auto">
+          <h1 className="text-center text-[29px] text-green mb-6 mt-4 leading-[35px]">
+            Login
+          </h1>
+          <img src={Tree} alt="Small" className="block sm:hidden" />
+          <img src={Tree2x} alt="Large" className="hidden sm:block h-32" />
+          <form className="flex flex-col mt-6 w-full">
+            <input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="outline-none bg-transparent border-b-2 border-b-[#E2E8F0] placeholder-green"
+            />
+            {!validEmail(email) && <Error />}
+            <input
+              type="password"
+              className="outline-none bg-transparent mt-5 border-b-2 border-b-[#E2E8F0] placeholder-green"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button className="text-[19px] text-white bg-green mt-6 h-[49px] rounded-full cursor-pointer">
+              Sign In
+            </button>
+          </form>
+          <div className="flex justify-between w-full my-4 text-sm">
+            <p>Forgot Password?</p>
+            <p className="text-[#D9185F]">New User? Sign Up</p>
+          </div>
+          <p>or</p>
+          <OAuthButton name="GOOGLE" bgColor="bg-[#4285F4]" />
+          <OAuthButton name="FACEBOOK" bgColor="bg-[#1877F2]" margin="mt-4" />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default LoginPage;
---
+++ src/components/OauthButton.tsx
@@ -0,0 +1,38 @@
+import Google from "/Google.png";
+import Google2x from "/Google2x.png";
+import Facebook from "/Facebook.png";
+import Facebook2x from "/Facebook2x.png";
+const OAuthButton = ({
+  name,
+  bgColor,
+  margin,
+}: {
+  name: "GOOGLE" | "FACEBOOK";
+  margin?: string;
+  bgColor: string;
+}) => {
+  return (
+    <button
+      className={`${bgColor} ${margin} relative w-full h-[46px] cursor-pointer text-white josefin-sans-600`}
+    >
+      <img
+        src={name === "GOOGLE" ? Google2x : Facebook2x}
+        width={name === "GOOGLE" ? 36 : 40}
+        height={name === "GOOGLE" ? 36 : 40}
+        className={`absolute ${
+          name === "GOOGLE" ? "left-1 top-[5px]" : "left-0.5 top-1"
+        } hidden sm:block`}
+      />
+      <img
+        src={name === "GOOGLE" ? Google : Facebook}
+        width={name === "GOOGLE" ? 36 : 40}
+        height={name === "GOOGLE" ? 36 : 40}
+        className={`absolute  ${
+          name === "GOOGLE" ? "left-1 top-[5px]" : "left-0.5 top-1"
+        } sm:hidden block`}
+      />
+      CONTINUE WITH {name}
+    </button>
+  );
+};
+export default OAuthButton;
